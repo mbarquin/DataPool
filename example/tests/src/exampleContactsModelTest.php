5da7dc9c3dff1f287c61bec0f48d72f5da8f8df0
mbarquin <moises.barquin@gmail.com>
2015-07-31 08:21:08 +0200

    FUnctional Testing. Failed, class is not indexing on array type access

--- example/tests/src/exampleContactsModelTest.php
+++ example/tests/src/exampleContactsModelTest.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace Example\tests\src;
 
 class exampleContactsModelTest extends \PHPUnit_Framework_TestCase {
 
+    /**
+     * @var \DataPool\DataPool Instance to be used while testing
+     */
     static private $dataPool = null;
-
 
     /**
      * Static instance of ContactsDataPool
@@ -12,21 +15,26 @@ class exampleContactsModelTest extends \PHPUnit_Framework_TestCase {
      * @return \Example\tests\files\ContactsDataPool
      */
     public function getDataPool() {
-        if(is_object(self::$dataPool) === false) {
+        // If not instanciated jet...
+        if (is_object(self::$dataPool) === false) {
             self::$dataPool = new \Example\tests\files\ContactsDataPool();
         }
+        // Avoid mixing behaviors during tests
+        self::$dataPool->setReturnArray(false);
+        self::$dataPool->setReturnIndexes(true);
+        
         return self::$dataPool;
     }
 
-
     public function testOneInsertOK() {
-        $dataPool        = $this->getDataPool();
-        $contModel       = new \Example\src\exampleContactsModel();
+        $dataPool = $this->getDataPool();
+        $contModel = new \Example\src\exampleContactsModel();
 
         $testInsertArray = $dataPool['Test1'];
+        var_dump($testInsertArray);
         unset($testInsertArray['result']);
 
-        $result          = $contModel->insert($testInsertArray);
+        $result = $contModel->insert($testInsertArray);
 
         $this->assertTrue(is_integer($result));
     }
@@ -35,19 +43,26 @@ class exampleContactsModelTest extends \PHPUnit_Framework_TestCase {
      * @dataProvider getDataPool
      */
     public function pureDataProviderInsert($name, $surname, $phone, $expected) {
-        $reg['name']    = $name;
+        $reg['name'] = $name;
         $reg['surname'] = $surname;
-        $reg['phone']   = $phone;
+        $reg['phone'] = $phone;
 
         $result = $contModel->insert($reg);
-        if($result === false) {
+        if ($result === false) {
             $this->assertEquals($expected, $result);
         } else {
             $this->assertTrue(is_integer($result));
         }
     }
 
-
+    public function getDataPoolAsArray() {
+        $dataPool = $this->getDataPool();
+        $dataPool->setReturnArray(true);
+        $dataPool->setReturnIndexes(true);
+        
+        return $dataPool;
+    }
+    
     /**
      * @dataProvider getDataPoolAsArray
      */
@@ -56,10 +71,11 @@ class exampleContactsModelTest extends \PHPUnit_Framework_TestCase {
         unset($regData['result']);
 
         $result = $contModel->insert($regData);
-        if($result === false) {
+        if ($result === false) {
             $this->assertEquals($expected, $result);
         } else {
             $this->assertTrue(is_integer($result));
         }
     }
+
 }
