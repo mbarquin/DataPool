14984339b943e5cd251744b3a604c7f26ad1cc33
Moi <moises.barquin@gmail.com>
2015-09-03 12:24:23 +0200

    Update exampleContactsModelTest.php

--- example/tests/src/exampleContactsModelTest.php
+++ example/tests/src/exampleContactsModelTest.php
@@ -11,30 +11,29 @@ class exampleContactsModelTest extends \PHPUnit_Framework_TestCase {
 
     /**
      * Static instance of ContactsDataPool
-     * 
+     *
      * @return \Example\tests\files\ContactsDataPool
      */
     public function getDataPool() {
-        // If not instanciated jet...
+        // If not instanciated yet...
         if (is_object(self::$dataPool) === false) {
             self::$dataPool = new \Example\tests\files\ContactsDataPool();
         }
         // Avoid mixing behaviors during tests
         self::$dataPool->setReturnArray(false);
         self::$dataPool->setReturnIndexes(false);
-        
+
         return self::$dataPool;
     }
 
     public function testOneInsertOKAndReset() {
-        $dataPool        = $this->getDataPool();
-        $dataPool->setReturnIndexes(TRUE);
-        
+        $dataPool = $this->getDataPool();
+        $dataPool->setReturnIndexes(true);
+
         $contModel       = new \Example\src\exampleContactsModel();
         $testInsertArray = $dataPool['Test1'];
-        
         $expected        = $testInsertArray['result'];
-        
+
         unset($testInsertArray['result']);
 
         $result = $contModel->insert($testInsertArray);
@@ -43,13 +42,43 @@ class exampleContactsModelTest extends \PHPUnit_Framework_TestCase {
         $contModel->reset();
         $this->assertAttributeCount(0, 'data', $contModel);
     }
-    
+
+    public function testNoArrayInsert() {
+        $contModel = new \Example\src\exampleContactsModel();
+        $result    = $contModel->insert('test');
+        $this->assertFalse($result);
+    }
+
+    public function testInsertParamsLostKO() {
+        $dataPool = $this->getDataPool();
+        $dataPool->setReturnIndexes(true);
+
+        $contModel       = new \Example\src\exampleContactsModel();
+        $testInsertArray = $dataPool['Test1'];
+        $expected        = $testInsertArray['result'];
+
+        unset($testInsertArray['result']);
+
+        $aux = $testInsertArray;
+        unset($aux['surname']);
+        $this->assertFalse($contModel->insert($aux));
+
+        $aux = $testInsertArray;
+        unset($aux['name']);
+        $this->assertFalse($contModel->insert($aux));
+
+        $aux = $testInsertArray;
+        unset($aux['phone']);
+        $this->assertFalse($contModel->insert($aux));
+    }
+
+
     /**
      * @dataProvider getDataPool
      */
     public function testPureDataProviderInsert($name, $surname, $phone, $expected) {
         $contModel      = new \Example\src\exampleContactsModel();
-        
+
         $reg['name']    = $name;
         $reg['surname'] = $surname;
         $reg['phone']   = $phone;
@@ -66,15 +95,17 @@ class exampleContactsModelTest extends \PHPUnit_Framework_TestCase {
         $dataPool = $this->getDataPool();
         $dataPool->setReturnArray(true);
         $dataPool->setReturnIndexes(true);
-        var_dump($dataPool->getRowsByIndex('Test'));
-        return $dataPool->getRowsByIndex('Test');
+        var_dump($dataPool->getRowsByIndex('Case'));
+        return $dataPool->getRowsByIndex('Case');
     }
-    
+
     /**
      * @dataProvider getDataPoolAsArray
      */
     public function testArrayDataProviderInsert($regData) {
-        $expected = $regData['result'];
+        $contModel      = new \Example\src\exampleContactsModel();
+        $expected       = $regData['result'];
+
         unset($regData['result']);
 
         $result = $contModel->insert($regData);
@@ -84,7 +115,5 @@ class exampleContactsModelTest extends \PHPUnit_Framework_TestCase {
             $this->assertTrue(is_integer($result));
         }
     }
-    
-    
 
 }
