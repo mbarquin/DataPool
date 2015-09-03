de2a4ba1f1c4ed3159261625075c3072613bfafe
Moi <moises.barquin@gmail.com>
2015-09-03 12:23:40 +0200

    Update ContactsDataPool.php

--- example/tests/files/ContactsDataPool.php
+++ example/tests/files/ContactsDataPool.php
@@ -30,13 +30,14 @@ class ContactsDataPool extends \DataPool\DataPool {
     );
 
     /**
-     * @var array Data array to be merged with definition and returned to tests 
+     * @var array Datasets array to be merged with definition and returned to tests
      */
     public $dataArray = array(
-        'Test1' => array('Jack', 'Travis', '555999666', true),
-        'Test2' => array('Mathew', 'Jones', '555888555', true),
+        'Test1'                  => array('Jack', 'Travis', '555999666', true),
+        'Test2'                  => array('Mathew', 'Jones', '555888555', true),
         'Test3.NameSurnameEmpty' => array('', '', '555666555', false),
-        'Case1.PhoneToLong' => array('Gregor', 'Jones', '5550005518899', false)
+        'TestCase1.PhoneToLong'  => array('Gregor', 'Jones', '5550005518899', false),
+        'TestCase2.NoName'       => array('', 'Lock', '555000559', false)
     );
 
 }
